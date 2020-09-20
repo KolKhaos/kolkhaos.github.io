3c268cbd186d4e5b597abc87066edbb6fa899c2f
KolKhaos <iasplund90@gmail.com>
2020-09-19 17:16:27 -0700

    changes to js

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,1 +1,11 @@
-document.getElementById('lastModified').innerHTML = `Last Updated: ${document.lastModified}`;
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    };
+    document.getElementById("lastModified").textContent = new Date().toLocaleDateString("en-us", options);
+} catch (e){
+    alert("Error with code or your browser does not support locale")
+}
