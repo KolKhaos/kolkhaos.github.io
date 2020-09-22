82d172d8297cd4db04e932b5b7f9d5eb295bad89
KolKhaos <iasplund90@gmail.com>
2020-09-21 17:47:03 -0700

    More possible fixes

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,12 +1,30 @@
-try {
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    }
-    document.getElementById("lastModified").textContent = new Date().toLocaleDateString("en-us", options);
-} 
-catch (e){
-    alert("Error with code or your browser does not support locale");
-}
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+document.getElementById("lastModified").textContent = fulldate;
