7834473f649041bd12dc64ee1ff2a2ec1f52203c
KolKhaos <iasplund90@gmail.com>
2020-12-01 22:48:27 -0800

    Final Web Page content and frame upload

---
+++ tuttut/js/prestonTT.js
@@ -0,0 +1,51 @@
+// Full Year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+//Hamburger
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+//Page Date
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("pageDate").textContent = fulldate;
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//pancakes
+let day = new Date();
+let pancake = document.querySelector("#pancake");
+
+if (day.getDay() == 5) {
+    pancake.style.display = "block";
+}
