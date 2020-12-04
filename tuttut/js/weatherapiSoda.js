20f3af3fbdbb4e971e23737378a2f7c16227f29e
KolKhaos <iasplund90@gmail.com>
2020-12-03 19:51:16 -0800

    JS updated and apis updated

---
+++ tuttut/js/weatherapiSoda.js
@@ -0,0 +1,23 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=966d8c0ed76ee5b0463da0dd4a1c228a"
+
+
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const currentTemp = document.querySelector('#current-temp');
+        const weatherIcon = document.querySelector('#icon');
+        const weatherImg = document.querySelector('#imagesrc');
+
+        currentTemp.innerHTML = jsObject.main.temp;
+
+        weatherImg.textContent = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+        const isrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+        weatherIcon.setAttribute('src', isrc);
+        weatherIcon.setAttribute('alt', jsObject.weather[0].description);
+
+    });
