ecf013b77b4ff0fb0116e8ca34ffbe770ce4375b
KolKhaos <iasplund90@gmail.com>
2020-12-16 07:06:12 -0800

    fixes to js additions to index and css!

--- scoots/js/scootsWeather.js
+++ scoots/js/scootsWeather.js
@@ -1,5 +1,4 @@
-const cozumelAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=20.499998&lon=-86.9499962&exclude=hourly,minutely,daily,alerts&units=imperial&appid=966d8c0ed76ee5b0463da0dd4a1c228a"
-const cozumelForeAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=20.499998&lon=-86.9499962&exclude=current,minutely,hourly,alerts&units=imperial&appid=966d8c0ed76ee5b0463da0dd4a1c228a";
+const cozumelAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=20.499998&lon=-86.9499962&exclude=hourly,minutely&units=imperial&appid=966d8c0ed76ee5b0463da0dd4a1c228a"
 
 
 
@@ -11,6 +10,9 @@ fetch(cozumelAPI)
         const weatherTemp = document.querySelector('#weatherTemp');
         const weatherFeels = document.querySelector('#weatherFeels');
         const weatherHum = document.querySelector('#weatherHum');
+        const forecastTemp0 = document.querySelector('#fore0');
+        const forecastTemp1 = document.querySelector('#fore1');
+        const forecastTemp2 = document.querySelector('#fore2');
 
 
         weatherState.innerHTML = jsObject.current.weather[0].main;
@@ -18,23 +20,17 @@ fetch(cozumelAPI)
         weatherFeels.innerHTML = jsObject.current.feels_like + "&#8457;";
         weatherHum.innerHTML = jsObject.current.humidity; + "&#37;";
 
+        forecastTemp0.innerHTML = jsObject.daily[0].temp.max + "&#8457;";
+        forecastTemp1.innerHTML = jsObject.daily[1].temp.max + "&#8457;";
+        forecastTemp2.innerHTML = jsObject.daily[2].temp.max + "&#8457;";
+
+        const weatherAlert = document.querySelector('#redAlert');
+        let alert = document.querySelector(".hg-alert");
+        if (jsObject.alert[i] >= 0) {
+            alert.style.display = "block"
+            weatherAlert.innerhtml = jsObject.alert[0].event;
+
+        };
+
+
     });
-
-fetch(cozumelForeAPI)
-    .then((response) => response.json())
-    .then((jsObject) => {
-            console.table(jsObject);
-
-            
-
-            for (let i = 0; i < jsObject.daily[2]; i++) {                
-                const forecastTemp0 = document.querySelector('#fore0');
-                const forecastTemp1 = document.querySelector('#fore1');
-                const forecastTemp2 = document.querySelector('#fore2');
-
-
-                forecastTemp0.innerHTML = jsObject.daily[0].max + "&#8457;";
-                forecastTemp1.innerHTML = jsObject.daily[1].max + "&#8457;";
-                forecastTemp2.innerHTML = jsObject.daily[2].max + "&#8457;";
-            }
-         });
